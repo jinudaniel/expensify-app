887870653c497d955e8ca1d3cdd7f972a5f98e1b
Jinu Daniel <jinudaniel74@gmail.com>
2018-12-16 20:25:03 +0530

    Update wbpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
     return {
-        entry: ".\\src\\app.js",
+        entry: "./src/app.js",
         output: {
             path: path.join(__dirname, 'public'),
             filename: 'bundle.js'
